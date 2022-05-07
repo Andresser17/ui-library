b16f94403ccffeee514a7690aa6bd3f15b5a3e26
Alejandro Serrano <andresserserrano2020@gmail.com>
2022-05-06 22:51:16 -0400

    Added loading-icon.svg and plus-icon.svg

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
 import Button from "./Button";
 
 function App() {
   return (
-    <div className="App p-4">
-      <Button text="HelloWorld" />
+    <div className="App p-4 bg-red-600">
+      <Button icon={true} rounded={false} loading={false} border={false} disabled={true} />
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -1,26 +1,45 @@
-import { useState } from "react";
+import PropTypes from "prop-types";
+import { ReactComponent as LoadingIcon } from "./icons/loading-icon.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus-icon.svg";
 
-function Button({ text }) {
-  const [mode, setMode] = useState("def");
-  // Modes
-  const def = "bg-[#6E41E2]";
-  const hover = "bg-[#5835B0]";
-  const active = "bg-[#472C8A]";
-  const focus = "bg-[#6E41E2] border-[#412A7E]";
-  const disabled = "bg-[#E3DAF9]";
-  // const loader = "bg-[#6E41E2]";
-  // Styles
-  const label = "";
-  // Designs
-  const primary = "bg-[#6E41E2] text-white";
+function Button({ text = "Enter", icon, rounded, border, loading, disabled }) {
+  const primary =
+    "bg-primary-bg text-primary-color hover:bg-primary-hover active:bg-primary-active focus:bg-primary-focus focus:border-primary-focusBorder disabled:bg-primary-disabled disabled:border-primary-disabledBorder disabled:text-primary-disabledColor";
+  const secondary =
+    "bg-secondary-bg text-secondary-color hover:bg-secondary-hover active:bg-secondary-active focus:bg-secondary-focus focus:border-secondary-focusBorder disabled:bg-secondary-disabled disabled:border-secondary-disabledBorder disabled:text-secondary-disabledColor";
+  const tertiary =
+    "bg-tertiary-bg text-tertiary-color hover:bg-tertiary-hover active:bg-tertiary-active focus:bg-tertiary-focus focus:border-tertiary-focusBorder disabled:bg-tertiary-disabled disabled:border-tertiary-disabledBorder disabled:text-tertiary-disabledColor";
+
+  const loadingIcon = (
+    <span className="mr-1 block w-5">
+      <LoadingIcon />
+    </span>
+  );
+  const customIcon = (
+    <span className="mr-1 block w-5">
+      <PlusIcon />
+    </span>
+  );
 
   return (
     <button
-      className={`bg-buttonDef text-white px-4 py-2 rounded-md hover:bg-buttonHover active:${active} focus:${focus} disabled:${disabled}`}
+      className={`px-4 py-2 flex items-centen focus:border-[0.125rem] rounded-sm ${
+        rounded && "rounded-full"
+      } ${primary} ${border && "border-2"}`}
+      disabled={disabled}
     >
+      {loading ? loadingIcon : icon && customIcon}
       {text}
     </button>
   );
 }
+Button.propTypes = {
+  text: PropTypes.string,
+  icon: PropTypes.bool,
+  rounded: PropTypes.bool,
+  border: PropTypes.bool,
+  loading: PropTypes.bool,
+  disabled: PropTypes.bool,
+};
 
 export default Button;
