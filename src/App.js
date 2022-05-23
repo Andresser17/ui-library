703155b597bb6538d2a61de614936950c013dd0d
Andresser17 <tensasangetsu1254@gmail.com>
2022-05-23 12:16:16 -0400

    Checkbox component styles are finished

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // Font Icons
-import "./icons/checkmark/styles.css";
+// import "./icons/checkmark/styles.css";
 // import { ReactComponent as LoadingIcon } from "./icons/loading-icon.svg";
 // import Button from "./Button";
 // import Badge from "./Badge";
@@ -8,8 +8,8 @@ import Checkbox from "./Checkbox";
 
 function App() {
   return (
-    <div className="App p-4 h-screen bg-gray-100 flex items-start justify-center">
-      <Checkbox indeterminate={false} disabled={false} />
+    <div className="App p-4 h-screen bg-green-400 flex items-start justify-center">
+      <Checkbox palette="primary" value="terror" indeterminate={false} error={false} disabled={false} />
     </div>
   );
 }
--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,25 +1,56 @@
 import { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
-import "./Checkbox.module.css"
+import checkStyle from "./Checkbox.module.css";
 
-function Checkbox({ value, palette, disabled, error, indeterminate }) {
-  const styles = "m-0 bg-white text-inherit grid place-content-center w-6 h-6 border-2 -translate-y-[0.075em] rounded-sm border-black appearance-none checked:scale-1"
-  const before = "before:w-2 before:h-2 before:scale-0 before:ease-in-out before:transition-transform before:shadow-inner before:shadow-red-600";
+function Checkbox({ onClick, value, palette, disabled, error, indeterminate }) {
+  // If palette is not provided, is equal primary
+  const optPalette = palette ? palette : "primary";
+  const styles = `${checkStyle.Checkbox} shadow-md rounded-sm w-5 h-5 disabled:opacity-[var(--disabled-opacity)]`;
+  // Unselected State
+  const unselected =
+    "bg-gray-100 hover:bg-gray-200 active:bg-gray-300 focus:bg-gray-100 focus:border focus:border-gray-400";
+  // Selected State
+  const selected =
+    "checked:bg-bg checked:hover:bg-hover checked:active:bg-active checked:focus:bg-focus checked:focus:border-focus-border";
+  // Indeterminate State
+  const inputIndeter =
+    "indeterminate:bg-bg indeterminate:hover:bg-hover indeterminate:active:bg-active indeterminate:focus:bg-focus indeterminate:focus:border-focus-border";
+  // Error state
+  const inputError = "bg-red-300";
+  const labelError = "text-red-300";
+  // Checkmark icon
+  const before = "before:bg-text";
   const checkbox = useRef();
 
-  // toggle indeterminate property
+  // Toggle indeterminate property
   useEffect(() => {
-    checkbox.current.indeterminate = indeterminate
+    checkbox.current.indeterminate = indeterminate;
   }, [indeterminate]);
 
   return (
-    <div className="flex justify-center items-center">
-      <input ref={checkbox} className="bg-red-600 w-5 h-5 text-text before:bg-gray-100 success" value={value} type="checkbox" disabled={disabled} />
-      <label className="ml-2">Hello</label>
+    <div className={`flex justify-center items-center ${optPalette}`}>
+      <input
+        onClick={onClick}
+        ref={checkbox}
+        className={`${styles} ${before} ${
+          error ? inputError : `${unselected} ${inputIndeter} ${selected}`
+        }`}
+        value={value}
+        type="checkbox"
+        disabled={disabled}
+      />
+      <label
+        className={`ml-2 text-text ${
+          disabled ? "opacity-[var(--disabled-opacity)]" : ""
+        } ${error ? labelError : ""}`}
+      >
+        Hello
+      </label>
     </div>
   );
 }
 Checkbox.propTypes = {
+  onClick: PropTypes.func,
   value: PropTypes.string,
   palette: PropTypes.string,
   disabled: PropTypes.bool,
